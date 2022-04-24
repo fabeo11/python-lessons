bc9a4953c82d223385f2128d572111f5703a9640
Fabio Masao <f.masaoishizu@gmail.com>
2022-04-24 09:52:02 -0300

    Feature de contador de rodadas e feature de dicas.

--- jogo_advinhacao/advinhacao.py
+++ jogo_advinhacao/advinhacao.py
@@ -2,18 +2,25 @@ print('**********************************************************')
 print('*				Jogo	da	adivinhação					*')
 print('**********************************************************')
 
-chute = int(input("Chute um número: "))
-print("Você digitou: ", chute)
+numero_tentativas = 3
+rodada = numero_tentativas
+num_secreto = 42
 
-num_secreto = 42;
-maior = (chute > num_secreto)
-menor = (chute < num_secreto)
-acertou = (num_secreto == chute)
+while(rodada > 0):
 
-if(acertou):
-    print("ACERTOU!")
-elif(maior):
-    print("Você errou, o número que escolheu é maior que o número secreto.")
-elif(menor):
-    print("Você errou, o número que escolheu é menor que o número secreto.")
+    print("\nTentativa {} de {}".format(rodada, numero_tentativas))
 
+    chute = int(input("Chute um número: "))
+    print("Você digitou: ", chute)
+
+    maior = (chute > num_secreto)
+    menor = (chute < num_secreto)
+    acertou = (num_secreto == chute)
+
+    if(acertou):
+        print("ACERTOU!")
+    elif(maior):
+        print("Você errou, o número que escolheu é maior que o número secreto.")
+    elif(menor):
+        print("Você errou, o número que escolheu é menor que o número secreto.")
+    rodada -= 1
